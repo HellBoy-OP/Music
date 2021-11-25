3c34eb4a6bf3b67df807fba31e25b4d52b0d06d3
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-25 21:50:16 +0530

    Update __main__.py

--- hellbot/__main__.py
+++ hellbot/__main__.py
@@ -1,12 +1,5 @@
-from pyrogram import Client
+from . import hellbot, run
 
-from .config import API_ID, API_HASH, BOT_TOKEN, HELLBOT_SESSION, SUDO_USERS
-
-
-hellbot = Client(":memory:", API_ID, API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="plugins"))
-
-client = Client(HELLBOT_SESSION, API_ID, API_HASH)
-run = client.run
 
 print("!!! HellBot Music Start-up Complete !!!")
 
