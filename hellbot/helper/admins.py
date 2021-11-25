1cea9cf134cf6deaacd3299c84ef8c20873f6f00
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-25 22:21:49 +0530

    Create admins.py

---
+++ hellbot/helper/admins.py
@@ -0,0 +1,27 @@
+from typing import List, Dict, Union
+from pyrogram.types import Chat, User
+
+admins: Dict[int, List[int]] = {}
+
+def set_ad(chat_id: int, admins_: List[int]):
+    admins[chat_id] = admins_
+
+def get_ad(chat_id: int) -> Union[List[int], bool]:
+    if chat_id in admins:
+        return admins[chat_id]
+    return False
+
+
+async def get_adminis(chat: Chat) -> List[User]:
+    get = get_ad(chat.id)
+    if get:
+        return get
+    else:
+        administrators = await chat.get_members(filter="administrators")
+        to_set = []
+        for administrator in administrators:
+            if administrator.can_manage_voice_chats:
+                to_set.append(administrator.user.id)
+
+        set_ad(chat.id, to_set)
+        return await get_administrators(chat)
