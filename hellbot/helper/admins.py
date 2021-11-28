5c9c6dfbd32423b10eed0c54267a37a804854fbf
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-28 17:29:09 +0530

    update admins.py

--- hellbot/helper/admins.py
+++ hellbot/helper/admins.py
@@ -1,5 +1,5 @@
 from typing import List, Dict, Union
-from pyrogram.types import Chat, User
+from pyrogram.types import Chat, User, Message
 
 admins: Dict[int, List[int]] = {}
 
@@ -25,3 +25,52 @@ async def get_admins(chat: Chat) -> List[User]:
 
         set_ad(chat.id, to_set)
         return await get_administrators(chat)
+
+
+async def extract_userid(message, text: str):
+    def is_int(text: str):
+        try:
+            int(text)
+        except ValueError:
+            return False
+        return True
+    text = text.strip()
+    if is_int(text):
+        return int(text)
+    entities = message.entities
+    app = message._client
+    if len(entities) < 2:
+        return (await app.get_users(text)).id
+    entity = entities[1]
+    if entity.type == "mention":
+        return (await app.get_users(text)).id
+    if entity.type == "text_mention":
+        return entity.user.id
+    return None
+
+
+async def extract_user_and_reason(message):
+    args = message.text.strip().split()
+    text = message.text
+    user = None
+    reason = None
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        if not reply.from_user:
+            return None, None
+        if len(args) < 2:
+            reason = None
+        else:
+            reason = text.split(None, 1)[1]
+        return reply.from_user.id, reason
+    if len(args) == 2:
+        user = text.split(None, 1)[1]
+        return await extract_userid(message, user), None
+    if len(args) > 2:
+        user, reason = text.split(None, 2)[1:]
+        return await extract_userid(message, user), reason
+    return user, reason
+
+
+async def extract_user(message):
+    return (await extract_user_and_reason(message))[0]
