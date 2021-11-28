fa94b1e414018d543aa355cd9593e0bb8e80f48b
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-29 02:45:26 +0530

    Update __main__.py

--- hellbot/__main__.py
+++ hellbot/__main__.py
@@ -4,10 +4,8 @@ from pathlib import Path
 
 from . import hellbot, run, load_plugins
 
+hellbot.start()
+run()
 
 print("!!! HellBot Music Start-up Complete !!!")
 from .plugins import admins, callbacks, chataction, groups, inline, ownercb, owners, play, start, youtube
-
-
-hellbot.start()
-run()
