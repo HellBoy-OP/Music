dd94b343c837dadc99d31e393b560ad403a9fa42
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-29 00:30:23 +0530

    Update __init__.py

--- hellbot/__init__.py
+++ hellbot/__init__.py
@@ -3,7 +3,7 @@ from pyrogram import Client
 from .config import API_HASH, API_ID, BOT_TOKEN, HELLBOT_SESSION
 
 
-hellbot = Client("HellBot Music", API_ID, API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="hellbot/plugins"))
+hellbot = Client("HellBot Music", API_ID, API_HASH, bot_token=BOT_TOKEN)
 client = Client(HELLBOT_SESSION, API_ID, API_HASH)
 
 run = client.run
