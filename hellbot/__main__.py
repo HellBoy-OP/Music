5baf8d77445bc12e9706b31b2392c7cbb3fb113e
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-29 02:04:47 +0530

    Update __main__.py

--- hellbot/__main__.py
+++ hellbot/__main__.py
@@ -2,14 +2,13 @@ import asyncio
 import glob
 import logging
 from pathlib import Path
-from pyrogram import idle
 
 from . import hellbot, run, load_plugins
 
 
 print("!!! HellBot Music Start-up Complete !!!")
 
-"""
+
 path = "hellbot/plugins/*.py"
 files = glob.glob(path)
 for name in files:
@@ -17,7 +16,6 @@ for name in files:
         patt = Path(a.name)
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
-"""
 
 hellbot.start()
 run()
