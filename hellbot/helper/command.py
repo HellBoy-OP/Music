ca60f721391ae1ccba82b75366f6c63ef4381e63
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-28 21:21:11 +0530

    Delete command.py

--- hellbot/helper/command.py
+++
@@ -1,9 +0,0 @@
-from typing import Union, List
-from pyrogram import filters
-
-grp_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
-pvt_filters = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
-
-
-def commandpro(commands: Union[str, List[str]]):
-    return filters.command(commands,"")
