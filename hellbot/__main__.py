8d16826ed03f025fe4f9babecc950ffa78203d68
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-29 02:39:18 +0530

    Update __main__.py

--- hellbot/__main__.py
+++ hellbot/__main__.py
@@ -1,4 +1,3 @@
-import asyncio
 import glob
 import logging
 from pathlib import Path
@@ -9,20 +8,5 @@ from . import hellbot, run, load_plugins
 print("!!! HellBot Music Start-up Complete !!!")
 
 
-path = "hellbot/plugins/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as a:
-        patt = Path(a.name)
-        plugin_name = patt.stem
-        load_plugins(plugin_name.replace(".py", ""))
-
 hellbot.start()
 run()
-
-"""
-async def process():
-    await hellbot.send_message(LOGGER_ID, "#START \n\nBot is now working")
-
-asyncio.get_event_loop().run_until_complete(process())
-"""
