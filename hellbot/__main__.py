4b564492abd28c21e0bd04fd174a5d76879ca035
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-29 00:34:07 +0530

    Update __main__.py

--- hellbot/__main__.py
+++ hellbot/__main__.py
@@ -1,7 +1,14 @@
 from . import hellbot, run
 
-
 print("!!! HellBot Music Start-up Complete !!!")
+
+path = "hellbot/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
 
 hellbot.start()
 run()
