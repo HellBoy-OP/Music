46cbd04b6ac56bbb66376cccdb1afa72e9db1434
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-29 11:20:11 +0530

    Update __main__.py

--- hellbot/__main__.py
+++ hellbot/__main__.py
@@ -1,26 +1,14 @@
-import glob
-import logging
-from pathlib import Path
+from pyrogram import Client as Bot
 
-from . import hellbot, client, run, load_plugins, group_call_instances
-
-
-@client.on_message(filters.me & filters.command("start"))
-async def pl(__, _):
-    if _.chat.id in group_call_instances.active_chats:
-        queues.put(_.chat.id, 'out.raw')
-    else:
-        await group_call_instances.set_stream(_.chat.id, 'out.raw')
+from .helper.pycalls import run
+from .config import API_HASH, API_ID, BOT_TOKEN
 
 
-@client.on_message(filters.me & filters.command("end"))
-async def pl(__, _):
-    await group_call_instances.stop(_.chat.id)
-
-
-hellbot.start()
-client.run()
+Bot(
+    'HellBot-Music',
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={'root': 'hellbot.plugins'},
+).start()
 run()
-
-print("!!! HellBot Music Start-up Complete !!!")
-
