7d46fcd2626f9a72edbcfc47f5b4e4febd7411da
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-29 11:14:51 +0530

    Update __main__.py

--- hellbot/__main__.py
+++ hellbot/__main__.py
@@ -2,7 +2,21 @@ import glob
 import logging
 from pathlib import Path
 
-from . import hellbot, client, run, load_plugins
+from . import hellbot, client, run, load_plugins, group_call_instances
+
+
+@client.on_message(filters.me & filters.command("start"))
+async def pl(__, _):
+    if _.chat.id in group_call_instances.active_chats:
+        queues.put(_.chat.id, 'out.raw')
+    else:
+        await group_call_instances.set_stream(_.chat.id, 'out.raw')
+
+
+@client.on_message(filters.me & filters.command("end"))
+async def pl(__, _):
+    await group_call_instances.stop(_.chat.id)
+
 
 hellbot.start()
 client.run()
