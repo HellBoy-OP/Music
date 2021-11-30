6b873d62ae31d140cd890dded174fefdf324a453
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-30 22:39:29 +0530

    2

--- hellbot/__init__.py
+++ hellbot/__init__.py
@@ -1,26 +1,14 @@
-import sys
-import logging
-import importlib
-
-from pathlib import Path
+from aiohttp import ClientSession
 from pyrogram import Client
+from Python_ARQ import ARQ
 
-from .config import API_HASH, API_ID, BOT_TOKEN, HELLBOT_SESSION
+from .config import API_HASH, API_ID, ARQ_API_URL, ARQ_API_KEY, BOT_TOKEN, HELLBOT_SESSION
 
 
 hellbot = Client("HellBot_Music", API_ID, API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="plugins"))
 client = Client(HELLBOT_SESSION, API_ID, API_HASH)
-
-
-def load_plugins(plugin_name):
-    path = Path(f"hellbot/plugins/{plugin_name}.py")
-    name = "hellbot.plugins.{}".format(plugin_name)
-    spec = importlib.util.spec_from_file_location(name, path)
-    load = importlib.util.module_from_spec(spec)
-    load.logger = logging.getLogger(plugin_name)
-    spec.loader.exec_module(load)
-    sys.modules["hellbot.plugins." + plugin_name] = load
-    print(f"🎶 Successfully Imported {plugin_name}")
+aiosession = ClientSession()
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiosession)
 
 
 run = client.run
