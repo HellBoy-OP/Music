f65aee74ac3232858f83b075748ad9f34c3549cf
DOMINATOR <92099157+dominator454@users.noreply.github.com>
2022-08-04 21:11:10 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 echo ">> FETCHING UPSTREAM..."
-git clone https://github.com/dominator454/Music_Bot /Music_Bot
+git clone https://github.com/The-HellBot/Music /Music
 echo ">> INSTALLING REQUIREMENTS..."
-cd /Music_Bot
+cd /Music
 pip3 install -U -r requirements.txt
 echo ">> STARTING MUSIC PLAYER USERBOT..."
 clear
