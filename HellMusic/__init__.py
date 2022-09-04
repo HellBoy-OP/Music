532a8379d1a52e822a5c2838489fe33253c17a80
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-09-04 15:09:39 +0530

    Update __init__.py

--- HellMusic/__init__.py
+++ HellMusic/__init__.py
@@ -1,7 +1,9 @@
 from HellMusic.core.voice import HellVoice
 from HellMusic.core.bot import MusicBot, MusicClient
+from HellMusic.core.database import HellMongoDB
 
 
 bot = MusicBot()
 client = MusicClient()
 hell = HellVoice()
+helldb = HellMongoDB()
