1d67e569d0dceb25e290c5d625252649f7026721
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-09-05 12:42:07 +0530

    Update and rename texts.py to text.py

---
+++ HellMusic/helpers/text.py
@@ -0,0 +1,9 @@
+DEPLOYED = "••• Hell-Music [{}] Started Successfully •••"
+
+HELP_TXT = ""
+
+NEW_PLAY = ""
+
+START_PM = ""
+
+START_GC = ""
--- HellMusic/helpers/texts.py
+++
@@ -1,9 +0,0 @@
-DEPLOYED = ""
-
-HELP_TXT = ""
-
-NEW_PLAY = ""
-
-START_PM = ""
-
-START_GC = ""
