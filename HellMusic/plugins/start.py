983366bc74da732adabfab743edd17b4e014a089
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2022-09-05 07:25:48 +0000

    fixes

--- HellMusic/plugins/start.py
+++ HellMusic/plugins/start.py
@@ -1,6 +1,6 @@
-from HellMusic import bot, hell, client, trg
 from pyrogram import filters
 from pyrogram.types import Message
+from HellMusic import bot, trg, hell, client
 
 
 @bot.on_message(filters.command(["start", "alive"], prefixes=trg) & filters.private)
