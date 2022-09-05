20be966e40d0ce1cd6dbc8d96a73e5872e36de5a
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2022-09-05 07:27:32 +0000

    fixes

--- HellMusic/__init__.py
+++ HellMusic/__init__.py
@@ -1,7 +1,8 @@
+from config import Config
 from HellMusic.core.voice import HellVoice
 from HellMusic.core.database import HellMongoDB
 from HellMusic.core.bot import MusicBot, MusicClient
-from config import Config
+
 
 bot = MusicBot()
 client = MusicClient()
