c88a65838817c76c2fd43c06f869f904c7b4b940
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-09-05 12:40:21 +0530

    Update __main__.py

--- HellMusic/__main__.py
+++ HellMusic/__main__.py
@@ -1,5 +1,29 @@
-from HellMusic import bot, hell, client
+import asyncio
+import sys
 
+from config import Config
+from pyrogram import idle
+from HellMusic import bot, hell, client, helldb
+from HellMusic.core.logging import LOGS
+from HellMusic.core.plugins import load_plugins
+from HellMusic.helpers.text import DEPLOYED
+from HellMusic import __version__
 
 async def startup():
-    pass
+    if not Config.HELLBOT_SESSION:
+        LOGS.error("[HELLBOT_SESSION]: Not a valid session!")
+        sys.exit()
+
+    LOGS.info("••• Music Bot Startup •••")
+
+    await bot.start()
+    await load_plugins()
+    await client.start()
+    await hell.start()
+
+    LOGS.info(DEPLOYED.format(__version__.version))
+    await idle()
+    LOGS.info("••• Hell-Music Stopped •••")
+
+
+asyncio.run(startup())
