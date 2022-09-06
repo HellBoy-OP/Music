b8167496c5910c5de668a61b05dc9d20e4ef0edf
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-09-06 13:22:11 +0530

    Create tools.py

---
+++ HellMusic/helpers/tools.py
@@ -0,0 +1,24 @@
+import asyncio
+import os
+import shlex
+from typing import Tuple
+
+
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(
+        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    return (
+        stdout.decode("utf-8", "replace").strip(),
+        stderr.decode("utf-8", "replace").strip(),
+        process.returncode,
+        process.pid,
+    )
+
+
+async def absolute_paths(directory):
+    for dirpath, _, filenames in os.walk(directory):
+        for f in filenames:
+            yield os.path.abspath(os.path.join(dirpath, f))
