69735ca3ecb59fc777ac8401a2d93e613a2cd27e
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-09-06 13:48:38 +0530

    Update text.py

--- HellMusic/helpers/text.py
+++ HellMusic/helpers/text.py
@@ -1,9 +1,13 @@
+CAPTION = "**✘ {}:** `{}` \n**✘ Views:** `{}` \n**✘ Duration:** `{}` \n\n**« ✘ »** {}"
+
 DEPLOYED = "••• Hell-Music [{}] Started Successfully •••"
 
 HELP_TXT = ""
 
 NEW_PLAY = ""
 
-START_PM = ""
+PERFORMER = "[ †hê Hêllẞø† ]"
 
 START_GC = ""
+
+START_PM = ""
