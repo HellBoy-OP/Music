ce8ac2607fdd834fa8b3b8688ab1c9690dee1509
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-09-06 13:18:13 +0530

    Update __init__.py

---
+++ HellMusic/plugins/__init__.py
@@ -0,0 +1,14 @@
+from HellMusic import bot, client
+
+
+# bot
+BOT_UN = bot.username
+BOT_ID = bot.id
+BOT_NAME = bot.name
+
+# client
+CLIENT_UN = client.username
+CLIENT_ID = client.id
+CLIENT_NAME = client.name
+
+#
