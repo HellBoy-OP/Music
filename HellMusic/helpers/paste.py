421b83b181a9f40c882fc2a42b5db138904d19b7
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-09-06 13:44:35 +0530

    Create paste.py

---
+++ HellMusic/helpers/paste.py
@@ -0,0 +1,18 @@
+from html_telegraph_poster import TelegraphPoster
+
+
+async def telegraph_paste(
+    title,
+    text,
+    auth="[ †he Hêllẞø† ]",
+    url="https://t.me/its_hellbot"
+):
+    client = TelegraphPoster(use_api=True)
+    client.create_api_token(auth)
+    post_page = client.post(
+        title=title,
+        author=auth,
+        author_url=url,
+        text=text,
+    )
+    return post_page["url"]
