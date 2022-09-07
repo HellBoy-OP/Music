b1e31329d2ec18999a0bc8f4f1096fb868c028aa
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-09-07 19:57:45 +0530

    Update start.py

--- HellMusic/plugins/start.py
+++ HellMusic/plugins/start.py
@@ -1,8 +1,18 @@
 from pyrogram import filters
 from pyrogram.types import Message
 from HellMusic import bot, trg, hell, client
+from HellMusic.helpers.text import START_GC, START_PM
 
 
-@bot.on_message(filters.command(["start", "alive"], prefixes=trg) & filters.private)
-async def start(bot, message: Message):
-    await message.reply_text("Alive!")
+@bot.on_message(
+    filters.command(["start", "alive"], prefixes=trg) & filters.private
+)
+async def start_pm(bot, message: Message):
+    await message.reply_text(START_PM)
+
+
+@bot.on_message(
+    filters.command(["start", "alive"], prefixes=trg) & ~filters.private
+)
+async def start_gc(bot, message: Message):
+    await message.reply_text(START_GC)
