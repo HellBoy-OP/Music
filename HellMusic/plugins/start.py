9b5e978e7b2df41258db261d613ff37c31ae7350
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2022-09-07 14:28:08 +0000

    fixes

--- HellMusic/plugins/start.py
+++ HellMusic/plugins/start.py
@@ -4,15 +4,11 @@ from HellMusic import bot, trg, hell, client
 from HellMusic.helpers.text import START_GC, START_PM
 
 
-@bot.on_message(
-    filters.command(["start", "alive"], prefixes=trg) & filters.private
-)
+@bot.on_message(filters.command(["start", "alive"], prefixes=trg) & filters.private)
 async def start_pm(bot, message: Message):
     await message.reply_text(START_PM)
 
 
-@bot.on_message(
-    filters.command(["start", "alive"], prefixes=trg) & ~filters.private
-)
+@bot.on_message(filters.command(["start", "alive"], prefixes=trg) & ~filters.private)
 async def start_gc(bot, message: Message):
     await message.reply_text(START_GC)
