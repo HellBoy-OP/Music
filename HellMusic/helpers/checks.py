6a602f008ff81886a550ac27370a11e0d32822a4
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-09-11 12:12:45 +0530

    Create checks.py

---
+++ HellMusic/helpers/checks.py
@@ -0,0 +1,12 @@
+from HellMusic import SUDO_USERS
+
+on_mode = ["on", "yes", "enable", "true"]
+
+async def check_mode(message):
+    mode = Config.PRIVATE_MODE
+    if mode.lower() in on_mode:
+        user = message.from_user.id
+        if user in SUDO_USERS:
+            return 1
+        else:
+            return 0
